a30124e5bf273c1d66aafc60249f8d034f1e4135
bkl <bogdan.klosek@fiz-karlsruhe.de>
2013-04-24 00:15:09 +0200

    first commit

---
+++ src/test/pkg4/Test4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.pkg4;
+
+/**
+ *
+ * @author bk
+ */
+public class Test4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
